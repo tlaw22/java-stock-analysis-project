3a239190760b7a12e1b495a340041357559484fd
tlaw22 <timothy.j.lawless@youtlook.com>
2024-01-07 09:50:10 -0500

    lesson complete

--- src/aggregators/AggregatorProcessor.java
+++ src/aggregators/AggregatorProcessor.java
@@ -1,6 +1,28 @@
 package aggregators;
 
-public class AggregatorProcessor {
-	
+import java.io.IOException;
+import java.util.List;
+
+import fileprocessors.StockFileReader;
+
+public class AggregatorProcessor <T extends Aggregator>{
+	T aggregator;
+    String file;
+    public AggregatorProcessor(T aggregator, String file) {
+        this.aggregator = aggregator;
+        this.file = file;
+    }
+public double runAggregator(int colIdx) throws IOException{
+     StockFileReader fileReader = new StockFileReader(file);
+     List<String> lines = fileReader.readFileData();
+     colIdx--;
+     for(String line : lines){
+        String [] numbers = line.split(",");
+        Number value = Double.parseDouble(numbers[colIdx]);
+        aggregator.add(value.doubleValue());
+     }
+     double number = aggregator.calculate();
+     return number;
+}
 	
 }
